51a35983a80bd73d6ef89edbc72e64f2dcd7ac0e
kevindoni <kevindoni17@gmail.com>
2021-03-19 17:12:16 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<header>
+	<title>Libernet Monitoring</title>
+	<link href="style.css" rel="stylesheet" />
+</header>
+
+<body>
+<div id="container">
+
+    <h1>Libernet Monitoring</h1>
+	<!-- Receive -->
+	<div id="rec_result"></div>
+	<canvas	id="rec_graph" width="400%" height="200"></canvas>
+	<!-- Send -->
+    <div id="snd_result"></div>
+     <canvas id="snd_graph" width="400%" height="200"></canvas>
+                              
+</div>
+
+<br>
+<div id="footer"></div>
+
+<script type="text/javascript" src="graph.js"></script>
+<script type="text/javascript" src="script.js"></script>
+
+</body>
+</html>
